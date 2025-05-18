08dcec109b6f4928945f0bb341ee6199fc182478
Eric Joldasov <bratishkaerik@landless-city.net>
2025-05-18 18:14:57 +0500

    chore: update to Zig 0.14
    Signed-off-by: Eric Joldasov <bratishkaerik@landless-city.net>

--- build.zig
+++ build.zig
@@ -8,22 +8,29 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "zig-system-zstd",
+    const main_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    exe.root_module.addImport("self", &exe.root_module);
+    main_mod.addImport("self", main_mod);
 
     const use_system_zstd = b.systemIntegrationOption("zstd", .{});
     switch (use_system_zstd) {
-        true => exe.root_module.linkSystemLibrary("zstd", .{}),
-        else => if (b.lazyDependency("zstd_from_source", .{})) |zstd_from_source| {
-            exe.root_module.linkLibrary(zstd_from_source.artifact("zstd"));
+        true => main_mod.linkSystemLibrary("zstd", .{}),
+        else => vendor_zstd: {
+            const zstd_from_source = b.lazyDependency(
+                "zstd_from_source",
+                .{ .target = target, .optimize = optimize },
+            ) orelse break :vendor_zstd;
+            main_mod.linkLibrary(zstd_from_source.artifact("zstd"));
         },
     }
 
+    const exe = b.addExecutable(.{
+        .name = "zig-system-zstd",
+        .root_module = main_mod,
+    });
     b.installArtifact(exe);
 }
